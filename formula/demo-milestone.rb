c5ee847a549f22a84d617fa57668daef1ac54ae3
root <>
2022-09-24 20:02:48 +0000

    Added release ${FORMULA_VERSION}

--- formula/demo-milestone.rb
+++ formula/demo-milestone.rb
@@ -4,7 +4,7 @@ class Emt < Formula
   desc "Enhanced Mule Tools CLI"
   homepage "https://www.enhanced-mule.com"
   url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.0-rc1-SNAPSHOT/enhanced-mule-tools-cli-@version@-dist.tbz2"
-  sha256 "foo"
+  sha256 "9d639c12c2b6caf01cd5ff994de6d352e6cd0b3d1c99168c068741bad8a87295  target/dodelme-1.0-rc1-SNAPSHOT-dist.tbz2"
   head "https://github.com/enhanced-mule/homebrew-tools.git"
 
   def install
