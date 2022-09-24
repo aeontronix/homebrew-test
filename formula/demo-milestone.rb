d07813302e9fbb675b5a79eb568d3740799acd56
root <>
2022-09-24 19:33:33 +0000

    Added release ${FORMULA_VERSION}

---
+++ formula/demo-milestone.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class Emt < Formula
+  desc "Enhanced Mule Tools CLI"
+  homepage "https://www.enhanced-mule.com"
+  url "https://repo1.maven.org/maven2/com/aeontronix/enhanced-mule/enhanced-mule-tools-cli/1.0-rc1-SNAPSHOT/enhanced-mule-tools-cli-@version@-dist.tbz2"
+  sha256 "@checksum@"
+  head "https://github.com/enhanced-mule/homebrew-tools.git"
+
+  def install
+    libexec.install "bin", "lib", "README.md"
+    bin.write_exec_script libexec/"bin/dodel"
+  end
+    test do
+      assert_match "1.0-rc1-SNAPSHOT", shell_output("#{bin}/dodel -V", 2)
+    end
+end
